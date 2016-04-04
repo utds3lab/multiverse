3ce8b8783ac4dd05eb2121f3f50d6140c39a7b6f
Erick Bauman <exb131030@utdallas.edu>
2016-04-04 01:50:51 -0500

    Fixed some instruction translation issues
    Right now, I think instructions actually translate correctly for
    conditional jumps, and unconditional jumps with immediate values.
    For the latter, I have implemented the special case for callbacks
    being passed to library functions resolved dynamically using the
    plt.  However, I also realized that the current approach for the
    brute-force disassembly technique does not work; the first byte
    of an instruction can't simply be replaced with its translation.
    This is a problem because immediately after that transformed
    instruction, if the remaining bytes of the instruction stay, they
    are now garbage bytes in this control flow (although starting
    from a different offset, they may be valid).  Therefore, somehow
    we need to efficiently store valid disassemblies for different
    offsets, and somehow be able to transition from one to another
    by using the mapping (a jump could switch to a different valid
    disassembly of the same code).  This requires more thought.

--- renabler.py
+++ renabler.py
@@ -39,9 +39,9 @@ def ELF32_R_SYM(val):
 #Globals: If there end up being too many of these, put them in a Context & pass them around
 plt = {}
 #TODO: Set actual address of function
-lookup_function_addr = 0
+lookup_function_offset = 0x8f
 #List of library functions that have callback args; each function in the dict has a list of
-#the arguments passed to it that are a callback (measured as the offset into the stack in bytes)
+#the arguments passed to it that are a callback (measured as the index of which argument it is)
 #TODO: Handle more complex x64 calling convention
 #TODO: Should I count _rtlf_fini (offset 5)?  It seems to be not in the binary
 callbacks = {'__libc_start_main':[0,3,4]}
@@ -52,8 +52,10 @@ callbacks = {'__libc_start_main':[0,3,4]}
 def remap_target(ins,mapping,target,offs): #Only works for statically identifiable targets
   newtarget = '0x8f'
   if mapping is not None and target in mapping:#Second pass, known mapping
-    newtarget = mapping[target]-mapping[ins.address]+offs #Offset from curr location in mapping
-    newtarget = '0x'+str(newtarget).encode('hex')
+    newtarget = mapping[target]-(mapping[ins.address]+offs) #Offset from curr location in mapping
+    newtarget = hex(newtarget)
+    print "original target: %s"%hex(target)
+    print "%s-(%s+%s) = %s"%(hex(mapping[target]),hex(mapping[ins.address]),hex(offs),newtarget)
   return newtarget
 
 def in_plt(target):
@@ -87,12 +89,8 @@ def get_callback_code(ins,mapping,name):
     cb_before = callback_template_before%ind
     code+=asm(cb_before) #Assemble the code to save the value at that stack offset to eax
     size=len(code) #Since jmp/call is relative, need the address we're coming from
-    target = '0x8f'
-    if mapping is not None:
-      #TODO: The offsets this produces seem wrong.  Analyze and evaluate.
-      target = lookup_function_addr-mapping[ins.address]+size
-      target = '0x'+str(target).encode('hex')
-    cb_after = callback_template_after%(target,ind)
+    lookup_target = remap_target(ins,mapping,lookup_function_offset,size)
+    cb_after = callback_template_after%(lookup_target,ind)
     code+=asm(cb_after)
   restore_eax = restore_reg_template%('eax','-12')
   code+=asm(restore_eax)
@@ -108,12 +106,19 @@ def translate_uncond(ins,mapping):
     target = op.imm
     callback_code = b'' #If this ends up not being a plt call with callbacks, add no code
     if in_plt(target):
-      print 'plt found: %s %s'%(ins.mnemonic,ins.op_str)
+      print 'plt found @%s: %s %s'%(hex(ins.address),ins.mnemonic,ins.op_str)
       entry = get_plt_entry(target)
       if entry is not None and entry in callbacks.keys():
         callback_code = get_callback_code(ins,mapping,entry)
     newtarget = remap_target(ins,mapping,target,len(callback_code))
-    return callback_code+asm(ins.mnemonic + ' $+' + newtarget)
+    print "(pre)new length: %s"%len(callback_code)
+    print "target: %s"%hex(target)
+    print "newtarget: %s"%newtarget
+    patched = asm(ins.mnemonic + ' $+' + newtarget)
+    if len(patched) == 2: #Short encoding, which we do not want
+      patched+='\x90\x90\x90' #Add padding of 3 NOPs
+    print "new length: %s"%len(callback_code+patched)
+    return callback_code+patched
   return None
 
 def translate_cond(ins,mapping):
@@ -126,7 +131,14 @@ def translate_cond(ins,mapping):
     #print ins.operands
     target = ins.operands[0].imm # int(ins.op_str,16) The destination of this instruction
     newtarget = remap_target(ins,mapping,target,0)
-    return asm(ins.mnemonic + ' $+' + newtarget)
+    patched = asm(ins.mnemonic + ' $+' + newtarget)
+    #TODO: some instructions encode to 6 bytes, some to 5, some to 2.  How do we know which?
+    #For example, for CALL, it seems to only be 5 or 2 depending on offset.
+    #But for jg, it can be 2 or 6 depending on offset, I think because it has a 2-byte opcode.
+    while len(patched) < 6: #Short encoding, which we do not want
+      patched+='\x90' #Add padding of NOPs
+    #print "(cond)new length: %s"%len(patched)
+    return patched
 
 def translate_one(ins,mapping):
   if ins.mnemonic in ['call','jmp']: #Unconditional jump
@@ -146,29 +158,44 @@ def translate_all(bytes,base):
     insts = md.disasm(bytes[off:off+15],base+off)#longest possible x86/x64 instr is 15 bytes
     try:
       ins = insts.next()
-      mapping[off+base] = newoff
+      mapping[base+off] = newoff
       newins = translate_one(ins,None)#In this pass, the mapping is incomplete
       if newins is not None:
+        #if off < 255:
+        #  print 'mapping %x to %x (and newins is %x)'%(off,newoff,len(newins))
+        #  print '%s'%newins.encode('hex')
+        #  tmps = md.disasm(newins,base+mapping[base+off])
+        #  for tmp in tmps:
+        #    print '0x%x(0x%x):\t%s\t%s'%(tmp.address,len(newbytes)+base,tmp.mnemonic,tmp.op_str)
         newoff+=len(newins)
       else:
         newoff+=1
     except StopIteration:
-      newoff+=1#Just move forward one byte
+      newoff+=1 #Just move forward one byte
+  #Now that the mapping is complete, we can add the mapping of the lookup function to the end
+  #TODO: Perhaps it would be more efficient if we guaranteed the function to be aligned?
+  global lookup_function_offset
+  lookup_function_offset = len(bytes)+base #Where we pretend it was in the old code (after the end)
+  mapping[len(bytes)+base] = newoff #Should be one after the last instruction in the new mapping
+  print 'lookup mapping %s:%s'%(hex(lookup_function_offset),hex(newoff+base))
   for off in range(0,len(bytes)):
     insts = md.disasm(bytes[off:off+15],base+off)#longest possible x86/x64 instr is 15 bytes
     try:
       ins = insts.next()
       newins = translate_one(ins,mapping)#The mapping is now complete
       if newins is not None:
-        newbytes+=newins #newins is simply the bytes of an assembled instruction
-        tmps = md.disasm(newins,base+off)
+        #print '%s'%newins.encode('hex')
+        tmps = md.disasm(newins,base+mapping[base+off])
+        print 'off: %x mapping[base+off]: %x len(newbytes): %x '%(off,mapping[base+off],len(newbytes))
         for tmp in tmps:
-          print '0x%x:\t%s\t%s'%(tmp.address,tmp.mnemonic,tmp.op_str)
+          print '0x%x(0x%x):\t%s\t%s'%(tmp.address,len(newbytes)+base,tmp.mnemonic,tmp.op_str)
         print '---'
+        newbytes+=newins #newins is simply the bytes of an assembled instruction
       else:
         newbytes+=bytes[off]
     except StopIteration:
       newbytes+=bytes[off] #No change, just add byte
+  #TODO: Right here append the actual code for the lookup function onto the end of newbytes
   return (mapping,newbytes)
 
 def renable(fname):
@@ -202,9 +229,26 @@ def renable(fname):
     print plt
     for seg in elffile.iter_segments():
       if seg.header['p_flags'] == 5 and seg.header['p_type'] == 'PT_LOAD': #Executable load seg
+        print "Base address: %s"%hex(seg.header['p_vaddr'])
         (mapping,newbytes) = translate_all(seg.data(),seg.header['p_vaddr'])
         md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
-        #print {k: (lambda x:x+addr)(v) for k,v in mapping.items()}
+        #insts = md.disasm(newbytes[0x8048360-seg.header['p_vaddr']:0x8048441-seg.header['p_vaddr']],0x8048360)
+        #The "mysterious" bytes between the previously patched instruction 
+        #(originally at 0x804830b) are the remaining bytes from that jmp instruction!
+        #So even though there was nothing between that jmp at the end of that plt entry
+        #and the start of the next plt entry, now there are 4 bytes from the rest of the jmp.
+        #This is a good example of why I need to take a different approach to generating the mapping.
+        insts = md.disasm(newbytes[0x80483af-seg.header['p_vaddr']:0x80483bf-seg.header['p_vaddr']],0x80483af)
+        for ins in insts:
+          print '0x%x:\t%s\t%s'%(ins.address,ins.mnemonic,ins.op_str)
+        tmpdct = {hex(k): (lambda x:hex(x+seg.header['p_vaddr']))(v) for k,v in mapping.items()}
+        keys = tmpdct.keys()
+        keys.sort()
+        output = ''
+        for key in keys:
+          output+='%s:%s '%(key,tmpdct[key])
+        #print output
+          
 '''
   with open(fname,'rb') as f:
     f.read(offs)
