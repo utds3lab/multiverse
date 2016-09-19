9ea83b8bf2af3f34cc82c187d1749864b4c445b5
Erick Bauman <exb131030@utdallas.edu>
2016-09-19 18:18:06 -0500

    Added code to handle dynamically loaded libraries to rewrite.py
    This is an old change that was not committed at the time I
    finished it.  If I recall correctly, a file listing all the
    dynamically loaded libraries (loaded after program start, e.g.
    with dlopen) is required so that those libraries can also be
    rewritten and passed to the dynamic linker so that it (hopefully)
    chooses those libraries instead of the originals.  However, this
    still does not work for (I think, but perhaps it might not work for
    other things too) loaded libraries that  have a fixed path to
    the library embedded in the application.  This discovery was the
    point that I started feeling existential despair, because it
    seems almost impossible to create a general way to guarantee the
    application will definitely use the rewritten libraries without
    replacing the originals...

--- rewrite.py
+++ rewrite.py
@@ -16,6 +16,17 @@ def extract_libraries(fname):
         paths.append(path)
   return paths
 
+def extract_dynamic_libraries(fname, libpath):
+  paths = []
+  dynlib = os.path.join(libpath, fname+'-dynamic-libs.txt')
+  if os.path.exists(dynlib):
+    with open(dynlib) as f:
+      path = f.readline()
+      while path != '':
+        paths.append(path.strip())
+        path = f.readline()
+  return paths
+
 def rewrite_libraries(libpath,paths):
   renabler.write_so = True
   for path in paths:
@@ -29,19 +40,42 @@ def rewrite_libraries(libpath,paths):
     shutil.move(libname+'-r-stat.json',libname+'-stat.json')
 
 if __name__ == '__main__':
-  if len(sys.argv) == 2:
-    print 'Getting required libraries for %s'%sys.argv[1]
-    paths = extract_libraries(sys.argv[1])
-
-    print 'Rewriting libraries'
-    (base,fname) = os.path.split(sys.argv[1])
+  if len(sys.argv) == 2 or len(sys.argv) == 3:
+    fpath = ''
+    dynamic_only = False
+    if len(sys.argv) == 2:
+      fpath = sys.argv[1]
+    else:
+      fpath = sys.argv[2]
+      if sys.argv[1] == '-d':
+        dynamic_only = True
+    
+    paths = []
+    
+    if not dynamic_only:
+      print 'Getting required libraries for %s'%fpath
+      paths = extract_libraries(fpath)
+    
+    (base,fname) = os.path.split(fpath)
     libpath = os.path.join(base,fname+'-libs-r')
     if not os.path.exists(libpath):
       os.makedirs(libpath)
+    print 'Getting dynamic libraries'
+    paths.extend(extract_dynamic_libraries(fname,libpath))
+    print 'Rewriting libraries'
     rewrite_libraries(libpath,paths)
-
-    print 'Rewriting main binary'
-    renabler.write_so = False
-    renabler.renable(sys.argv[1])
+    
+    if not dynamic_only:
+      print 'Rewriting main binary'
+      renabler.write_so = False
+      renabler.renable(fpath)
+    
+    print 'Writing runnable .sh'
+    with open(fpath+'-r.sh', 'w') as f:
+      ld_preload = ''
+      for path in extract_dynamic_libraries(fname,libpath):
+        (lbase,lname) = os.path.split(path)
+        ld_preload += os.path.join(libpath,lname) + ' '
+      f.write('#!/bin/bash\nLD_LIBRARY_PATH=./%s LD_BIND_NOW=1 LD_PRELOAD="%s" ./%s'%( fname+'-libs-r', ld_preload, fname+'-r' ) )
   else:
     print "Error: must pass executable filename.\nCorrect usage: %s <filename>"%sys.argv[0]
