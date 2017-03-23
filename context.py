6a060fbb1cd608a5972eca750ad3f6a3340c9cea
Erick Bauman <exb131030@utdallas.edu>
2017-03-22 21:11:15 -0500

    Added translator object and context object
    The translator will be used to translate all instructions
    from an original binary to their corresponding instructions in
    a rewritten binary.  This refactoring of code will allow for
    (hopefully) easier modification of the translation to support
    both x86 and x86-64 binaries without adding too much complexity.
    
    The context object is a collection of what used to be global
    variables, now collected into one object.  I'm not sure about
    whether this is really the best design decision, but I have not
    yet thought of a better way to organize the assorted global
    variables.

---
+++ context.py
@@ -0,0 +1,35 @@
+
+class Context(object):
+
+  def __init__(self):
+    self.plt = {}
+    self.newbase = 0x09000000
+    self.lookup_function_offset = 0x8f
+    self.secondary_lookup_function_offset = 0x8f #ONLY used when rewriting ONLY main executable
+    self.mapping_offset = 0x8f
+    self.global_sysinfo = 0x8f	#Address containing sysinfo's address
+    self.global_flag = 0x8f
+    self.global_lookup = 0x7000000	#Address containing global lookup function
+    self.popgm = 'popgm'
+    self.popgm_offset = 0x8f
+    self.new_entry_off = 0x8f
+    self.write_so = False
+    self.exec_only = False
+    self.no_pic = False
+    self.get_pc_thunk = None
+    self.stat = {}
+    self.stat['indcall'] = 0
+    self.stat['indjmp'] = 0
+    self.stat['dircall'] = 0
+    self.stat['dirjmp'] = 0
+    self.stat['jcc'] = 0
+    self.stat['ret'] = 0
+    self.stat['origtext'] = 0
+    self.stat['newtext'] = 0
+    self.stat['origfile'] = 0
+    self.stat['newfile'] = 0
+    self.stat['mapsize'] = 0
+    self.stat['lookupsize'] = 0
+    #List of library functions that have callback args; each function in the dict has a list of
+    #the arguments passed to it that are a callback (measured as the index of which argument it is)
+    self.callbacks = {'__libc_start_main':[0,3,4]}
---
+++ translator.py
@@ -0,0 +1,21 @@
+
+class Translator(object):
+  ''' A Translator converts the original instructions from a source
+      binary into their corresponding translated instructions for
+      the rewritten binary.  This includes translating addresses
+      for jmp/JCC/call/ret destinations and inserting user-defined
+      instrumentation code around instructions. 
+
+      This is a generic Translator object.  All translator
+      used by this system should inherit from this parent
+      object and provide implementations for all functions listed.'''
+  def __init__(self,before_callback):
+    raise NotImplementedError('Override __init__() in a child class')
+  def translate_one(ins,mapping):
+    raise NotImplementedError('Override translate_one() in a child class')
+  def translate_uncond(ins,mapping):
+    raise NotImplementedError('Override translate_uncond() in a child class')
+  def translate_cond(ins,mapping):
+    raise NotImplementedError('Override translate_cond() in a child class')
+  def translate_ret(ins,mapping):
+    raise NotImplementedError('Override translate_ret() in a child class')
