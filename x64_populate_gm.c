31151638bb0a978ce3c67bcf37dcd83c5c90c1da
Erick Bauman <exb131030@utdallas.edu>
2017-07-27 21:55:37 -0500

    Finished x64 popgm
    Now it should be functional enough to start writing and testing
    the global lookup function and other x64 code for rewriting both
    the main binary and libraries.

--- x64_populate_gm.c
+++ x64_populate_gm.c
@@ -35,7 +35,7 @@ struct gm_entry {
 
 unsigned int __attribute__ ((noinline)) my_read(int, char *, unsigned int);
 int __attribute__ ((noinline)) my_open(const char *);
-void populate_mapping(unsigned int, unsigned int, unsigned int, unsigned int, struct gm_entry *);
+void populate_mapping(unsigned int, unsigned long, unsigned long, unsigned long, struct gm_entry *);
 void process_maps(char *, struct gm_entry *);
 struct gm_entry lookup(unsigned long, struct gm_entry *);
 
@@ -69,9 +69,12 @@ int _start(struct gm_entry *global_mapping){
 
 #ifdef DEBUG
 	printf("READ:\n%s\n", buf);
-	// simulation for testing - dont call process maps
-	populate_mapping(1, 0x08800000, 0x08880000, 0x07000000, global_mapping);
-	populate_mapping(2, 0x09900000, 0x09990000, 0x07800000, global_mapping);
+	process_maps(buf,global_mapping);
+	int items = global_mapping[0].lookup_function;
+	// simulation for testing
+	populate_mapping(items + 0, 0x08800000, 0x08880000, 0x07000000, global_mapping);
+	populate_mapping(items + 1, 0x09900000, 0x09990000, 0x07800000, global_mapping);
+	global_mapping[0].lookup_function += 2;//Show that we have added these
 	/*
 	int i;
 	for (i = 0x08800000; i < 0x08880000; i++){
@@ -102,15 +105,20 @@ int _start(struct gm_entry *global_mapping){
 #ifdef DEBUG
 struct gm_entry lookup(unsigned long addr, struct gm_entry *global_mapping){
 	unsigned int index;
-	//unsigned long gm_size = global_mapping[0].lookup_function;//Size is stored in first entry
+	unsigned long gm_size = global_mapping[0].lookup_function;//Size is stored in first entry
 	global_mapping++;//Now we point at the true first entry
-	//Use binary search on the already-sorted entries (doing linear search for testing purposes)
-	for(index = 0; index < 10; index++){
+	//Use binary search on the already-sorted entries
+	//Here is a linear search for simple testing purposes.
+	//For small arrays, binary search may not be as useful, so I may for now just use linear search.
+	//I can try using binary search later and doing a performance comparison.
+	//However, if I want to do binary search, I should do a conditional mov to reduce the number of branches
+	for(index = 0; index < gm_size; index++){
 		//printf("SEARCHING 0x%lx :: mapping[%d] :: 0x%lx :: 0x%lx :: 0x%lx\n", addr, index, global_mapping[index].lookup_function, global_mapping[index].start, global_mapping[index].length);
 		if( addr - global_mapping[index].start <= global_mapping[index].length){
 			printf("0x%lx :: mapping[%d] :: 0x%lx :: 0x%lx :: 0x%lx\n", addr, index, global_mapping[index].lookup_function, global_mapping[index].start, global_mapping[index].length);
 		}
 	}
+	
 	return global_mapping[index];
 }
 #endif
@@ -127,7 +135,7 @@ unsigned int __attribute__ ((noinline)) my_read(int fd, char *buf, unsigned int 
 		"mov %0, rax\n"
 		: "=g" (bytes_read)
 		: "g" ((long)fd), "g" (buf), "g" ((long)count)
-		: "rcx", "r11"
+		: "rax", "rdi", "rsi", "rdx", "rcx", "r11"
 	);
 	return (unsigned int) bytes_read;
 }
@@ -179,22 +187,16 @@ char *next_line(char *line){
 	return NULL;
 }
 
-unsigned int my_atol(char *a){
+unsigned long my_atol(char *a){
 	/*
 	 * convert unknown length (max 16) hex string into its integer representation
 	 * assumes input is from /proc/./maps
 	 * i.e., 'a' is a left-padded 16 byte lowercase hex string
 	 * e.g., "000000000804a000"
 	 */
-	/*unsigned long i = 0;
-	int place, digit;
-	for (place = 15; place >= 0; place--, a++){
-		digit = (int)(*a) - 0x30;
-		if (digit > 9)
-			digit -= 0x27; // digit was [a-f]
-		i += (unsigned long) digit << (place << 2);
-	}
-	return i;*/
+#ifdef DEBUG
+	//printf("Converting string to long: \"%s\"\n", a);
+#endif
 	unsigned long l = 0;
 	unsigned char digit = *a;
 	while( (digit >= '0' && digit <= '9') || (digit >= 'a' && digit <= 'f') ){
@@ -202,11 +204,15 @@ unsigned int my_atol(char *a){
 		if( digit > 9 ) digit -= 0x27; // digit was hex character
 		l <<= 4; // Shift by half a byte
 		l += digit;
+		digit = *(++a);
 	}
+#ifdef DEBUG
+	//printf("Resulting value: %lx\n", l);
+#endif
 	return l;
 }
 
-void parse_range(char *line, unsigned int *start, unsigned int *end){
+void parse_range(char *line, unsigned long *start, unsigned long *end){
 	/* 
 	 * e.g., "08048000-08049000 ..."
 	 * Unfortunately, for 64-bit applications, the address ranges do not have a
@@ -218,10 +224,13 @@ void parse_range(char *line, unsigned int *start, unsigned int *end){
 	*end   = my_atol(line+1);
 }
 
-void populate_mapping(unsigned int gm_index, unsigned int start, unsigned int end, unsigned int lookup_function, struct gm_entry *global_mapping){
+void populate_mapping(unsigned int gm_index, unsigned long start, unsigned long end, unsigned long lookup_function, struct gm_entry *global_mapping){
 	global_mapping[gm_index].lookup_function = lookup_function;
 	global_mapping[gm_index].start = start;
 	global_mapping[gm_index].length = end - start;
+#ifdef DEBUG
+	printf("Added gm entry @ %d: (0x%lx, 0x%lx, 0x%lx)\n", gm_index, global_mapping[gm_index].lookup_function, global_mapping[gm_index].start, global_mapping[gm_index].length);
+#endif
 }
 
 void process_maps(char *buf, struct gm_entry *global_mapping){
@@ -233,28 +242,31 @@ void process_maps(char *buf, struct gm_entry *global_mapping){
 	unsigned int gm_index = 1;//Reserve first entry for metadata
 	unsigned char permissions = 0;
 	//unsigned int global_start, global_end;
-	unsigned int old_text_start, old_text_end;
-	unsigned int new_text_start, new_text_end;
+	unsigned long old_text_start, old_text_end;
+	unsigned long new_text_start, new_text_end;
 
-	//Assume global mapping is first entry at 0x7000000 and that there is nothing before
-	//TODO: In 64-bit applications 0x7000000 will not come before the main executable,
-	//so we must follow a different approach! 
-	//Skip global mapping
+	//Assume global mapping is first entry at 0x200000 and that there is nothing before
+	//Skip global mapping (put at 0x200000 in 64-bit binaries, as opposed to 0x7000000 for x86)
 	line = next_line(line);
-	permissions = get_permissions(line);
 	do{ // process each block of maps
+		permissions = get_permissions(line);
 		// process all segments from this object under very specific assumptions
 		if ( is_exec(permissions) ){
 			if( !is_write(permissions) ){
 				parse_range(line, &old_text_start, &old_text_end);
+#ifdef DEBUG
+				printf("Parsed range for r-xp: %lx-%lx\n", old_text_start, old_text_end);
+#endif
 			}else{
 				parse_range(line, &new_text_start, &new_text_end);
+#ifdef DEBUG
+				printf("Parsed range for rwxp: %lx-%lx\n", new_text_start, new_text_end);
+#endif
 				populate_mapping(gm_index, old_text_start, old_text_end, new_text_start, global_mapping);
 				gm_index++;
 			}
 		}
 		line = next_line(line);
-		permissions = get_permissions(line);
 	} while(line != NULL);
 	// assume the very last executable and non-writable segment is that of the dynamic linker (ld-X.X.so)
 	// populate those ranges with the value 0x00000000 which will be compared against in the global lookup function
@@ -266,9 +278,11 @@ void process_maps(char *buf, struct gm_entry *global_mapping){
 
 #ifdef DEBUG
 int main(void){
-	void *mapping_base = (void *)0x09000000;
+	void *mapping_base = (void *)0x200000;
+	void *new_section = (void *)0x8000000;
 	int fd = open("/dev/zero", O_RDWR);
-	void *global_mapping = mmap(mapping_base, 0x400000, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	void *global_mapping = mmap(mapping_base, 0x10000, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	mmap(new_section, 0x4000, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 0); //Create a mock new "text" section that would be added by process_maps
 	if (global_mapping != mapping_base){
 		printf("failed to get requested base addr\n");
 		exit(1);
